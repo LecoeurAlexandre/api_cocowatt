0a4151309e06ab88a70734f3cf84dcb258314279
Alexandre Lecoeur <alexandre.lecoeur.dev@gmail.com>
2023-09-13 11:54:08 +0200

    stat in progress

--- adapters/src/main/java/com/example/adapters/controller/StatController.java
+++ adapters/src/main/java/com/example/adapters/controller/StatController.java
@@ -1,4 +1,34 @@
 package com.example.adapters.controller;
 
+import com.example.adapters.entity.StatDTO;
+import com.example.domain.port.CarService;
+import com.example.domain.port.ReservationService;
+import com.example.domain.port.TripService;
+import com.example.domain.port.UserService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("api/stats")
 public class StatController {
+    private final UserService userService;
+
+    private final ReservationService reservationService;
+    private final TripService tripService;
+    private final CarService carService;
+
+    public StatController(UserService userService, ReservationService reservationService, TripService tripService, CarService carService) {
+        this.userService = userService;
+        this.reservationService = reservationService;
+        this.tripService = tripService;
+        this.carService = carService;
+    }
+
+    @GetMapping()
+    public ResponseEntity get() {
+        StatDTO statDTO = new StatDTO();
+        int driversCount = userService.driversCount();
+    }
 }
--- adapters/src/main/java/com/example/adapters/entity/StatDTO.java
+++ adapters/src/main/java/com/example/adapters/entity/StatDTO.java
@@ -2,9 +2,11 @@ package com.example.adapters.entity;
 
 import lombok.AllArgsConstructor;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @AllArgsConstructor
+@NoArgsConstructor
 public class StatDTO {
     private int drivers;
     private int travellers;
